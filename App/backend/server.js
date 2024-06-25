907040678de8afd4313944a9104a8059461c2067
Manish Sahni <Manish-Sahni>
2024-06-25 19:03:59 +0530

    some another changes

--- App/backend/server.js
+++ App/backend/server.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true }
-);
+mongoose.connect(uri);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
