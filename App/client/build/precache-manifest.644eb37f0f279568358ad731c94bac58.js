e1d2938bb03e999cf2afe582374fd8c109c3c8d0
Manish Sahni <Manish-Sahni>
2024-06-25 18:54:28 +0530

    some changes

---
+++ App/client/build/precache-manifest.644eb37f0f279568358ad731c94bac58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a554a4fad2710be6d2275ef60c3c8365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa2977280989949d9d9",
+    "url": "/static/css/2.e7089bb7.chunk.css"
+  },
+  {
+    "revision": "baa2977280989949d9d9",
+    "url": "/static/js/2.1394880b.chunk.js"
+  },
+  {
+    "revision": "a505e67bed30d0d891897a0122666995",
+    "url": "/static/js/2.1394880b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04f669ecad42cfe28278",
+    "url": "/static/js/main.d684480f.chunk.js"
+  },
+  {
+    "revision": "46b79f41f618af2fdb54",
+    "url": "/static/js/runtime-main.f720f733.js"
+  }
+]);
